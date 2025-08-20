2edf36169f801b04af461fb5ca93ed3e76780d3c
HassanHGGameil <hgprand@gmail.com>
2025-08-20 14:31:57 +0300

    updatde to first deplyment

--- src/lib/constains/constains.ts
+++ src/lib/constains/constains.ts
@@ -1,5 +1,5 @@
-// export const DOMAIN = `https://qasr-alsutan-dashboard-lfc5.vercel.app` ;
-export const DOMAIN = `http://localhost:3000`;
+export const DOMAIN = `https://qasr-alsultan-dashboard.vercel.app` ;
+// export const DOMAIN = `http://localhost:3000`;
 
 // FRONT END
 export const FRONTEND = `http://localhost:3001`;
