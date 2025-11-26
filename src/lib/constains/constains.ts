3094b5361b96f560e4a18b081b64e4e73ae773a6
HassanHGGameil <hgprand@gmail.com>
2025-11-26 10:40:19 +0200

    updated

--- src/lib/constains/constains.ts
+++ src/lib/constains/constains.ts
@@ -1,5 +1,5 @@
 // export const DOMAIN = `https://qasr-alsultan-dashboard.vercel.app` ;
 // export const DOMAIN = `http://localhost:3000`;
 // export const DOMAIN = `http://72.60.80.158:3000`;
-export const DOMAIN = `http://markupagency.net`;
+export const DOMAIN = `https://markupagency.net`;
 export const FRONTEND = `http://localhost:3001`;
