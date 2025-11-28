cec7e0be38a59327dbec4d3814c29f6ff7a5bbfe
HassanHGGameil <hgprand@gmail.com>
2025-11-28 04:51:51 +0200

    updatd this concepots

--- src/lib/constains/constains.ts
+++ src/lib/constains/constains.ts
@@ -4,23 +4,23 @@
 // ______ Locale Running ______
 // export const DOMAIN = `http://localhost:3000`;
 // export const STORE = `http://localhost:3000`;
-// export const FRONTEND = `http://72.60.80.158:3000`;
+// export const FRONTEND = `http://localhost:3000`;
 
 
 
 // ______ VPS Production ______
-export const DOMAIN = `http://72.60.80.158`;
-export const STORE = `http://72.60.80.158`;
-export const FRONTEND = `http://72.60.80.158`;
+// export const DOMAIN = `http://72.60.80.158`;
+// export const STORE = `http://72.60.80.158`;
+// export const FRONTEND = `http://72.60.80.158`;
 
 
 
 
 
 // ______ Main Production ______
-// export const DOMAIN = `http://markupagency.net`;
-// export const STORE = `http://markupagency.net`;
-// export const FRONTEND = `http://markupagency.net`;
+export const DOMAIN = `http://markupagency.net`;
+export const STORE = `http://markupagency.net`;
+export const FRONTEND = `http://markupagency.net`;
 
 
 
